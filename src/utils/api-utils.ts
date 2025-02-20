e132231f342bf8062339f78f404af6f8b7fe4ab8
Philip Dayboch <philip245@gmail.com>
2025-02-20 09:09:38 -0500

    Update API URL to use api.localhost
    This updates the API URL in the development environment to use
    api.localhost which nginx is configured to listen for on the dev
    servers. Production will still use api.tincan.com, but we'll
    need to update that once the app is open on the public internet.
    For now, the app is being used only on a local network which has
    manual DNS entries for tincan.com.

--- src/utils/api-utils.ts
+++ src/utils/api-utils.ts
@@ -1,5 +1,5 @@
 export const getBaseApiUrl = (): string => {
   if (process.env.NODE_ENV === 'production') return 'http://api.tincan.com'
-  
-  return 'http://127.0.0.1:3005';
+
+  return 'http://api.localhost';
 };
